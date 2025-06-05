6273e2767b5a8d44883bc993b338aa6ae2754b95
Inder Grewal <inderdeepg8@gmail.com>
2025-06-05 13:51:54 -0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,18 @@
+import "/src/styles.css";
+
+if (true) {
+  console.log("yes!");
+}
+
+console.log("test");
+
+/* 
+Run Webpack:
+    npm run build
+
+Run server:
+    npm run dev
+
+Run tests:
+    npm run watch
+*/
